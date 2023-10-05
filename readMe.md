4ac8c3ab40dfaf81af6187f4aad49a729309b77a
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-10-05 10:00:26 +0200

    add readMe

---
+++ readMe.md
@@ -0,0 +1,48 @@
+# Vitrine3p
+
+Vitrine3p est un site internet promouvant des formations professionnelles. C'est un site vitrine dans lequel l'admin peut modifier les membres de son équipe et son catalogue de formation.
+
+## Requirements
+
+- PHP 8.2
+- Symfony 6.3.5
+- MySql
+
+- CKEditor5
+
+## Tokens
+
+Tokens générés via JWT pour la vérification de l'adresse e-mail et la modification du mot de passe
+
+## Bundles
+
+- EasyAdminBundle 4
+
+## User
+
+### Vérification adresse e-mail user
+- Services JWTService.php et SendMailService.php
+- dans config/package/messenger commenter   # Symfony\Component\Mailer\Messenger\SendEmailMessage: async
+- dans env.local mettre le secret JWT
+- dans env.local modifier le mailer : MAILER_DSN=smtp://localhost:1025
+
+### Modification mdp et mdp oublié user
+- Services JWTService.php et SendMailService.php
+
+### Photos du catalogue et des membres de l'équipe
+- utilisation d'un service PictureService.php
+
+## Description d'une formation avec CKEditor5
+
+- télécharger le dossier sur https://ckeditor.com/ckeditor-5/download/?null-addons= 
+- l'installer dans public/assets/ckeditor5
+- créer un fichier ckeditor.js dans assets/js
+- mise en forme de la toolbar et des textes des headings dans ckeditor.js
+- dans formationType.php mettre le champs description en HiddenType
+- dans formation/edit.html.twig, créer le formulaire, mettre une div avec "editor" en id
+- dans formation/edit.html.twig, créer un block js avec lien vers ckeditor.js du dossier ckeditor5 et du dossier js
+- pour que la mise en forme apparaisse, mettre {{ formation.description | raw  }} dans formation show 
+
+## License
+
+MIT Licence
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,38 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Service\JWTService;
 use App\Security\Authenticator;
 use App\Security\EmailVerifier;
+use App\Service\SendMailService;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    private EmailVerifier $emailVerifier;
-
-    public function __construct(EmailVerifier $emailVerifier)
-    {
-        $this->emailVerifier = $emailVerifier;
-    }
-
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, Authenticator $authenticator, EntityManagerInterface $entityManager): Response
-    {
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        UserAuthenticatorInterface $userAuthenticator,
+        Authenticator $authenticator,
+        EntityManagerInterface $entityManager,
+        JWTService $jWTService,
+        SendMailService $sendMailService,
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setRoles(['ROLE_USER']);
+
             // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -46,45 +52,126 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('contact@vitrine3p.fr', 'Vitrine3P Ne pas répondre'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
             // do anything else you need here, like send an email
+
+
+            // Génération du JWT de l'utilisateur
+            // créer le header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+            //créer le payload
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+            //définir la durée de validité en nb de secondes
+            $validity = 86400;
+            // générer le token
+            $token = $jWTService->generate($header, $payload, $this->getParameter('app.jwtsecret'), $validity);
+            //envoi d'un mail
+            $sendMailService->send(
+                'no-reply@vitrine3p.fr',
+                $user->getEmail(),
+                'Activation de votre compte vitrine3p',
+                'register',
+                compact('user', 'token')
+            );
+
+            $this->addFlash('success', 'Félicitations ! Votre compte vient d\'être créé. Un e-mail vient de vous être envoyé à l\'adresse que vous nous avez communiquée. Merci de cliquer sur le lien d\'activation afin de finaliser votre inscription.');
 
             return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
                 $request
             );
+        } else {
+            return $this->render('registration/register.html.twig', [
+                'registrationForm' => $form->createView(),
+                'passError' => 'Les mots de passe ne sont pas identiques.'
+            ]);
         }
-
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
     }
 
-    #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    #[Route('/verify/{token}', name: 'app_verify_user')]
+    public function verifyUser(
+        $token,
+        JWTService $jWTService,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        // vérifier si le token est valide, n'a pas expiré et n'a pas été modifié
+        if ($jWTService->isValidToken($token) && !$jWTService->isExpiredToken($token) && $jWTService->checkSignatureToken($token, $this->getParameter('app.jwtsecret'))) {
+            // récupération du payload
+            $payload = $jWTService->getPayload($token);
+            // récupération du user du token
+            $user = $userRepository->find($payload['user_id']);
+            //vérification que le user existe et n'a pas encore activé so compte
+            if ($user && !$user->isVerified()) {
+                $user->setIsVerified(true);
+                $entityManager->flush($user);
 
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+                // dd($user);
+                $this->addFlash('success', 'Félicitations ! Votre compte est activé !');
+                return $this->redirectToRoute('app_user_index');
+            }
+        }
+        // ici un problème dans le token
+        $this->addFlash('danger', 'Le Token est invalide ou a expiré.');
+        return $this->redirectToRoute('app_login');
+    }
 
-            return $this->redirectToRoute('app_register');
+    // renvoi de la vérification
+    #[Route('/renvoiverif', name: 'app_resend_verif')]
+    public function resendVerif(
+        JWTService $jWTService,
+        SendMailService $sendMailService,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $user = $this->getUser();
+
+        // vérifie que l'utilisateur est connecté
+        if (!$user) {
+            $this->addFlash('danger', 'Vous devez être connecté pour accéder à cette page.');
+            return $this->redirectToRoute('app_login');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        //vérifie que l'utilisateur n'a pas déjà été vérifié
+        //dd($user->getIsVerified());
+        if ($user->isVerified()) {
+            $this->addFlash('warning', 'Ce compte utilisateur est déjà activé.');
+            return $this->redirectToRoute('app_user_index');
+        }
 
-        return $this->redirectToRoute('app_register');
+        // Génération du JWT de l'utilisateur
+        // créer le header
+        $header = [
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ];
+
+        //créer le payload
+        $payload = [
+            'user_id' => $user->getId()
+        ];
+        //définir la durée de validité en nb de secondes
+        $validity = 86400;
+
+        // générer le token
+        $token = $jWTService->generate($header, $payload, $this->getParameter('app.jwtsecret'), $validity);
+
+        // dd($token);
+
+        //envoi d'un mail
+        $sendMailService->send(
+            'no-reply@vitrine3p.fr',
+            $user->getEmail(),
+            'Activation de votre compte vitrine3p.fr',
+            'register',
+            compact('user', 'token')
+        );
+        $this->addFlash('success', 'Un e-mail vient de vous être envoyé à l\'adresse que vous nous avez communiquée.');
+        return $this->redirectToRoute('app_user_index');
     }
 }
