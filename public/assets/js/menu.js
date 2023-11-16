65500532c1a50202ac4385c25ea3cade55c8ad3e
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-11-16 13:49:30 +0100

    mise en forme menu desktop et mobile

---
+++ public/assets/js/menu.js
@@ -0,0 +1,15 @@
+console.log("hello menu.js");
+
+const navbarMenu = document.querySelector(".navbar-menu");
+const burger = document.querySelector(".burger");
+const closeBtn = document.querySelector(".close");
+const showHideNavbar = () => {
+  navbarMenu.classList.toggle("hideNav");
+  navbarMenu.classList.toggle("showNav");
+  closeBtn.classList.toggle("dnone");
+  burger.classList.toggle("dnone");
+};
+burger.addEventListener("click", showHideNavbar);
+closeBtn.addEventListener("click", showHideNavbar);
+
+console.dir(closeBtn);
